34e92e75d4d925b69c4a8011d0c43f9a8b466489
carla <carlafzaraujo@gmail.com>
2024-05-14 11:23:31 -0300

    Criação do backend e frontend

--- Third_Solution/btk_executavel.py
+++ Third_Solution/btk_executavel.py
@@ -1,5 +1,5 @@
 from customtkinter import *
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, simpledialog
 from PIL import Image
 from proposta_btk import criar_pdf
 import os
@@ -12,7 +12,6 @@ app.configure(background_color='#131313')
 side_img_data = Image.open('Third_Solution\Templates\side_image.png')
 side_img = CTkImage(dark_image=side_img_data, light_image=side_img_data, size=(300, 480))
 CTkLabel(master=app, text="", image=side_img).pack(expand=True, side="left")
-
 frame = CTkFrame(master=app, width=300, height=480, fg_color='#131313')
 frame.pack_propagate(0)
 frame.pack(expand=True, side="right")
@@ -31,16 +30,21 @@ def select_save_location():
 
 # Função para gerar o PDF
 def generate_pdf():
-    excel_file = excel_path_label.cget("text")  # Obtenha o caminho do arquivo Excel
-    save_path = save_path_label.cget("text")    # Obtenha o local de salvamento
+    excel_file = excel_path_label.cget("text")
+    save_path = save_path_label.cget("text")
     if excel_file and save_path:
-        try:
-            criar_pdf(excel_file, os.path.join(save_path, 'Proposta_Comercial.pdf'))
-            messagebox.showinfo('Sucesso', 'Seu PDF foi gerado com sucesso! Confira o local de salvamento para ver seu PDF')
-        except Exception as e:
-            messagebox.showerror('Erro', 'Falha ao gerar PDF. Certifique-se de que o arquivo está no diretório correto')
+        file_name = simpledialog.askstring("Salvar como", "Digite o nome do arquivo:", parent=app)
+        if file_name:
+            full_path = os.path.join(save_path, f"{file_name}.pdf")
+            try:
+                criar_pdf(excel_file, full_path)
+                messagebox.showinfo('Sucesso', 'Seu PDF foi gerado com sucesso!')
+            except Exception as e:
+                messagebox.showerror('Erro', 'Falha ao gerar PDF.')
+        else:
+            messagebox.showinfo('Informação', 'Operação cancelada.')
     else:
-        print("Por favor, selecione o arquivo Excel e o local de salvamento.")
+        messagebox.showerror('Erro', 'Por favor, selecione o arquivo Excel e o local de salvamento.')
 
 CTkLabel(master=frame, text='BTK SOLUTIONS', text_color="#F27F1B", anchor="w", justify="left", font=("Arial Bold", 24)).pack(anchor="w", pady=(50, 5), padx=(25, 0))
 CTkLabel(master=frame, text="Orçamento automático em PDF", text_color="#7E7E7E", anchor="w", justify="left", font=("Arial Bold", 12)).pack(anchor="w", padx=(25, 0))
